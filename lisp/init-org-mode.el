c6003c9bfccb96768c2c25a2d4bfc626f5ef5d9d
Tony Ampomah <tony@arksolutions.it>
2021-08-09 11:27:45 +0100

    Update org roam path

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -274,7 +274,7 @@
   :init
   (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/Notes/Roam")
+  (org-roam-directory "~/Nextcloud/Notes/Roam")
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
 	 ("C-c n g" . org-roam-graph)

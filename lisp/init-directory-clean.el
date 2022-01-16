893d6098249502324cfbde016af8909068cc5ad4
Tony Ampomah <tony@arksolutions.it>
2022-01-16 14:45:10 +0000

    Stops emacs from literring with lockfiles

--- lisp/init-directory-clean.el
+++ lisp/init-directory-clean.el
@@ -1,9 +1,9 @@
 ;;; init-directory-clean.el --- Keep Emacs Directory Clean -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
+(setq create-lockfiles nil)
 (setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
-(setq make-backup-files nil)
+;; (setq make-backup-files nil)
 
 ;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
 (setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
@@ -11,6 +11,11 @@
 
 ;; Use no-littering to automatically set common paths to the new user-emacs-directory
 (use-package no-littering)
+
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
 
 (provide 'init-directory-clean)
 ;;; init-directory-clean.el ends here

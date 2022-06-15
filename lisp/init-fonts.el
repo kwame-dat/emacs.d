2fa629711cc47fe0d2ea2e7c9f054c968e70e758
Tony Ampomah <tony@arksolutions.it>
2022-06-15 05:57:00 +0100

    Update fonts config

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,55 +1,6 @@
 ;;; init-fonts.el --- Fonts -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
-(setq kd/default-font "Cascadia Code")
-(setq kd/default-font-size 14)
-(setq kd/current-font-size kd/default-font-size)
-
-(setq kd/font-change-increment 1.1)
-
-(defun kd/font-code ()
-  (concat kd/default-font "-" (number-to-string kd/current-font-size)))
-
-(defun kd/set-font-size ()
-  "Set the font to `kd/default-font' at `kd/current-font-size'.
-  Set that for the current frame, and also make it the default for
-  other, future frames."
-  (let ((font-code (kd/font-code)))
-    (if (assoc 'font default-frame-alist)
-        (setcdr (assoc 'font default-frame-alist) font-code)
-      (add-to-list 'default-frame-alist (cons 'font font-code)))
-    (set-frame-font font-code)))
-
-(defun kd/reset-font-size ()
-  "Change font size back to `kd/default-font-size'."
-  (interactive)
-  (setq kd/current-font-size kd/default-font-size)
-  (kd/set-font-size))
-
-(defun kd/increase-font-size ()
-  "Increase current font size by a factor of `kd/font-change-increment'."
-  (interactive)
-  (setq kd/current-font-size
-        (ceiling (* kd/current-font-size kd/font-change-increment)))
-  (kd/set-font-size))
-
-(defun kd/decrease-font-size ()
-  "Decrease current font size by a factor of `kd/font-change-increment', down to a minimum size of 1."
-  (interactive)
-  (setq kd/current-font-size
-        (max 1
-             (floor (/ kd/current-font-size kd/font-change-increment))))
-  (kd/set-font-size))
-
-(define-key global-map (kbd "C-0") 'kd/reset-font-size)
-(define-key global-map (kbd "C-+") 'kd/increase-font-size)
-(define-key global-map (kbd "C-=") 'kd/increase-font-size)
-(define-key global-map (kbd "C-_") 'kd/decrease-font-size)
-(define-key global-map (kbd "C--") 'kd/decrease-font-size)
-
-(kd/reset-font-size)
-
 (setq-default line-spacing 0.8)
 (set-face-attribute 'default nil :font "Cascadia Code Light")
 (set-face-attribute 'fixed-pitch nil :family "Noto Sans" :height 1.0)

29b25aa6f015e1f2a8035232944785f55071def6
Tony Ampomah <tony@arksolutions.it>
2022-08-18 15:56:02 +0100

    Switch to doom themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,18 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(straight-use-package 'modus-themes)
+;; (straight-use-package 'modus-themes)
+(straight-use-package 'doom-themes)
 
 ;; mode-line
-(setq modus-themes-mode-line '(accented borderless padded))
+;; (setq modus-themes-mode-line '(accented borderless padded))
 
-(setq modus-themes-region '(bg-only))
+;; (setq modus-themes-region '(bg-only))
 
-(setq modus-themes-bold-constructs t)
-(setq modus-themes-italic-constructs t)
-(setq modus-themes-paren-match '(bold intense))
+;; (setq modus-themes-bold-constructs t)
+;; (setq modus-themes-italic-constructs t)
+;; (setq modus-themes-paren-match '(bold intense))
+;; (load-theme 'modus-vivendi t)
 
-(load-theme 'modus-vivendi t)
+(load-theme 'doom-tomorrow-night t)
 
 (provide 'init-themes)
 ;;; init-themes.el ends here

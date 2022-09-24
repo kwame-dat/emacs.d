8186ffc36f0ce3e0fefa891a98652fc7faf14058
Tony Ampomah <tony@arksolutions.it>
2022-09-24 10:17:52 +0100

    Update org-roam notes path

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -294,7 +294,7 @@
   :init
   (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/org/Notes")
+  (org-roam-directory "~/Notes")
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
 	 ("C-c n g" . org-roam-graph)

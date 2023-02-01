6af6ab00f6549d9d2b405613e0fd923f5246308e
Tony Ampomah <tony@arksolutions.it>
2023-02-01 21:26:09 +0000

    Activate editorconfig

--- lisp/init-editorconfig.el
+++ lisp/init-editorconfig.el
@@ -1,10 +1,10 @@
 ;;; init-editorconfig.el --- editorconfig -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-;; (use-package editorconfig
-;;   ;; :defer t
-;;   :config
-;;   (editorconfig-mode 1))
+(use-package editorconfig
+  ;; :defer t
+  :config
+  (editorconfig-mode 1))
 
 (provide 'init-editorconfig)
 ;;; init-editorconfig.el ends here

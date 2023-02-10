44ee76e9b1a0d06d02544f1bde2a19893927f238
Tony Ampomah <tony@arksolutions.it>
2023-02-10 23:15:21 +0000

    Revert "Integrate with open terminal for macos"
    This reverts commit ca3ff40f609cbf752d2a6babbd5861886e379465.

--- lisp/init-terminal.el
+++ lisp/init-terminal.el
@@ -102,7 +102,7 @@
 		     (projectile-project-root)
 		   (projectile-project-root))))
     (call-process-shell-command
-     (concat "open -a Warp.app "workdir) nil 0)))
+     (concat "alacritty --working-directory  "workdir) nil 0)))
 
 
 (straight-use-package 'ob-tmux)

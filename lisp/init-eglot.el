94b899b1048e61c64616ebd56130454ccc3a7e2b
Tony Ampomah <tony@arksolutions.it>
2023-06-04 10:28:37 +0100

    Add eglot

---
+++ lisp/init-eglot.el
@@ -0,0 +1,14 @@
+;;; init-eglot.el --- eglot -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+(use-package eglot
+  :straight t)
+
+(with-eval-after-load "eglot"
+  (add-to-list 'eglot-server-programs '(php-mode "intelephense" "--stdio"))
+  (add-to-list 'eglot-stay-out-of 'eldoc)
+  (with-eval-after-load "php-mode"
+    (define-key php-mode-map (kbd "<f5>") #'eldoc-box-eglot-help-at-point)))
+
+(provide 'init-eglot)
+;;; init-eglot.el ends here

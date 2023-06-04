a4295cb30448a7be86a5a4d0a2ca1c24a0b9da59
Tony Ampomah <tony@arksolutions.it>
2023-06-04 10:14:24 +0100

    Update php mode config

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -4,20 +4,9 @@
 (use-package php-mode
   :mode "\\.inc\\'"
   :defer t
-  :hook (php-mode . rainbow-delimiters-mode)
+  :hook (php-mode . visual-line-mode)
   :config
   (setq php-mode-template-compatibility nil))
-
-(require 'lsp-diagnostics)
-(lsp-diagnostics-flycheck-enable)
-(require 'flycheck)
-(defun my-flycheck-setup ()
-  (flycheck-add-next-checker 'lsp 'php-phpcs 'php-md))
-(add-hook 'php-mode-hook #'my-flycheck-setup)
-
-(use-package phpactor
-  :defer t
-  :commands (phpactor-install-or-update))
 
 (use-package phpunit
   :after php-mode

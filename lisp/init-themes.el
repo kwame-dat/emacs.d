164a31811545f7bdc80461818ffde13eb76dedee
Tony Ampomah <tony@arksolutions.it>
2023-09-26 19:09:15 +0100

    Update theme config

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -6,17 +6,16 @@
 (straight-use-package 'doom-themes)
 
 ;; mode-line
-;; (setq modus-themes-mode-line '(accented borderless padded))
+(setq modus-themes-mode-line '(accented borderless padded))
 
-;; (setq modus-themes-region '(bg-only))
+(setq modus-themes-region '(bg-only))
 
-;; (setq modus-themes-bold-constructs t)
-;; (setq modus-themes-italic-constructs t)
-;; (setq modus-themes--weight-widget 'light)
-;; (setq modus-themes-paren-match '(bold intense))
-;; (load-theme 'modus-vivendi t)
+(setq modus-themes-bold-constructs t)
+(setq modus-themes-italic-constructs t)
+(setq modus-themes--weight-widget 'light)
+(setq modus-themes-paren-match '(bold intense))
+(load-theme 'modus-vivendi t)
 
-(load-theme 'doom-tomorrow-night t)
 
 (provide 'init-themes)
 ;;; init-themes.el ends here

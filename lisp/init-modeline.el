a65503b80b52a333d315f87ab3d5214332ed4812
Tony Ampomah <tony@arksolutions.it>
2023-09-26 19:15:57 +0100

    Modeliine tweaks

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,10 +1,30 @@
 ;;; init-modeline.el --- Modline -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-(setq doom-modeline-modal-icon t)
+(use-package nerd-icons)
+
 (use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode))
+  :hook (after-init . doom-modeline-mode)
+  :custom
+  (doom-modeline-height 25)
+  (doom-modeline-bar-width 1)
+  (doom-modeline-icon t)
+  (doom-modeline-modal-icon t)
+  (doom-modeline-major-mode-icon t)
+  (doom-modeline-major-mode-color-icon t)
+  (doom-modeline-buffer-file-name-style 'truncate-upto-project)
+  (doom-modeline-buffer-state-icon t)
+  (doom-modeline-buffer-modification-icon t)
+  (doom-modeline-minor-modes nil)
+  (doom-modeline-enable-word-count nil)
+  (doom-modeline-buffer-encoding t)
+  (doom-modeline-indent-info nil)
+  (doom-modeline-checker-simple-format t)
+  (doom-modeline-vcs-max-length 12)
+  (doom-modeline-env-version t)
+  (doom-modeline-irc-stylize 'identity)
+  (doom-modeline-github-timer nil)
+  (doom-modeline-gnus-timer nil))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here

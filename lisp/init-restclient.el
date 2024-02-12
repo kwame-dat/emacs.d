bf50e0c3b27e5d60f5e1501a60e294ee2492a38a
Tony Ampomah <tony@arksolutions.it>
2024-02-12 09:34:32 +0000

    Add jq-mode + restclient-jq packages

--- lisp/init-restclient.el
+++ lisp/init-restclient.el
@@ -1,6 +1,8 @@
 ;;; init-restclient.el --- restclient -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
+(use-package jq-mode)
+(use-package restclient-jq)
 (use-package restclient
   :defer t)
 (provide 'init-restclient)

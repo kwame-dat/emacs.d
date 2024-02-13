b9ea6d943fc24c87dcc7ccb104997ab111b7c040
Tony Ampomah <tony@tony.ampomah>
2024-02-13 12:52:47 +0000

    Remove all-the-icons package

--- lisp/init-icons.el
+++ lisp/init-icons.el
@@ -2,13 +2,5 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package all-the-icons
-  :defer t)
-
-(use-package all-the-icons-ivy
-  :defer t
-  :config
-  (all-the-icons-ivy-setup))
-
 (provide 'init-icons)
 ;;; init-icons.el ends here

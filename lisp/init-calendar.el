220a52835925ed51b39aaa0b23b1d56e409fcace
Tony Ampomah <tony@arksolutions.it>
2024-02-27 18:43:47 +0000

    Enable org calendar

--- lisp/init-calendar.el
+++ lisp/init-calendar.el
@@ -3,24 +3,12 @@
 ;;; Code:
 
 (setq calendar-week-start-day 1)
-;; (use-package calfw)
-;; (use-package calfw-org)
-;; (use-package calfw-cal)
-;; (use-package calfw-ical)
+(use-package calfw)
+(use-package calfw-org)
+(use-package calfw-cal)
 
-;; (defun my-open-calendar ()
-;;   (interactive)
-;;   (cfw:open-calendar-buffer
-;;    :contents-sources
-;;    (list
-;;     (cfw:org-create-source "Green")  ; orgmode source
-;;     (cfw:cal-create-source "Orange") ; diary source
-;;     (cfw:ical-create-source "Moon" "~/moon.ics" "Gray")  ; ICS source1
-;;     (cfw:ical-create-source "gcal" "https://..../basic.ics" "IndianRed") ; google calendar ICS
-;;     ))) 
-
-;; (kd/leader-key-def
-;;   "cc"  '(cfw:open-org-calendar :which-key "calendar"))
+(kd/leader-key-def
+  "cc"  '(cfw:open-org-calendar :which-key "calendar"))
 
 (provide 'init-calendar)
 ;;; init-calendar.el ends here

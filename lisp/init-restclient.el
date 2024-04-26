d45620d02bf50abd2242d2899375753df08d6bb3
Tony Ampomah <tony@arksolutions.it>
2024-04-26 14:47:01 +0100

    Update restclient config

--- lisp/init-restclient.el
+++ lisp/init-restclient.el
@@ -2,8 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 (use-package jq-mode)
+(use-package restclient)
 (use-package restclient-jq)
-(use-package restclient
-  :defer t)
 (provide 'init-restclient)
 ;;; init-restclient.el ends here

2d3088b4bd27f8443e81b7aeaa38a82714b0192a
Tony Ampomah <tony@arksolutions.it>
2024-11-14 22:39:34 +0000

    Updating theme back to doom themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,20 +2,9 @@
 ;;; Commentary:
 ;;; Code:
 
-(straight-use-package 'modus-themes)
 (straight-use-package 'doom-themes)
 
-;; mode-line
-(setq modus-themes-mode-line '(accented borderless padded))
-
-(setq modus-themes-region '(bg-only))
-
-(setq modus-themes-bold-constructs t)
-(setq modus-themes-italic-constructs t)
-(setq modus-themes--weight-widget 'light)
-(setq modus-themes-paren-match '(bold intense))
-;; (load-theme 'doom-one-light t)
-(load-theme 'modus-vivendi t)
+(load-theme 'doom-tomorrow-night t)
 
 
 (provide 'init-themes)

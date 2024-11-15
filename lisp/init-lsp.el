ac2b672f9d4d58da09a73d230b5e22d7cfd3dd90
Tony Ampomah <tony@arksolutions.it>
2024-11-15 21:59:01 +0000

    Update lsp mode config

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -2,11 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (setq lsp-log-io nil) ;; Don't log everything = speed
+(setq lsp-log-io nil) ;; Don't log everything = speed
 ;; (setq lsp-restart 'auto-restart)
-;; (setq lsp-ui-sideline-show-diagnostics t)
-;; (setq lsp-ui-sideline-show-hover nil)
-;; (setq lsp-ui-sideline-show-code-actions t)
+(setq lsp-ui-sideline-show-diagnostics t)
+(setq lsp-ui-sideline-show-hover nil)
+(setq lsp-ui-sideline-show-code-actions t)
 ;; (setq lsp-ui-sideline-enable t)
 ;; (setq lsp-ui-doc-position 'bottom)
 (setq lsp-idle-delay 0.500)
@@ -15,11 +15,13 @@
 
 (use-package lsp-mode
   :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (XXX-mode . lsp)
-         ;; if you want which-key integration
+  :hook (
+         (php-mode . lsp)
+         (python-mode . lsp)
+         (js2-mode . lsp)
+         (web-mode . lsp)
+         (typescript-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
@@ -28,8 +30,6 @@
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 (use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
 
 (kd/leader-key-def
   "l"  '(:ignore t :which-key "lsp")
